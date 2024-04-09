3ef62a00c5fef98b96621177687eaad7d1b22aa5
Satish Nande <satishnande23@gmail.com>
2024-04-09 14:51:21 +0530

    debug

--- main.py
+++ main.py
@@ -31,7 +31,7 @@ amazonHeaders = {"User-Agent":"Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:66.0
                 "Connection":"close", 
                 "Upgrade-Insecure-Requests":"1"}
 
-flipkartHeaders = {"User-Agent":"Mozilla/5.0 (Windows NT 10.0; Win64; x64) Gecko/20100101 Firefox/66.0", 
+flipkartHeaders = {"User-Agent":"Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:66.0) Gecko/20100101 Firefox/66.0", 
                     # "Accept-Encoding":"gzip, deflate", 
                     # "Accept":"text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8",
                     # "DNT":"1",
