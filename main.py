7561e1a971c2c79e0d7690c097853a06673e9e4e
snande <satishnande23@gmail.com>
2024-07-17 17:59:06 +0530

    flipkart header

--- main.py
+++ main.py
@@ -39,7 +39,7 @@ amazonHeaders = {"User-Agent":"Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:66.0
                 "Connection":"close", 
                 "Upgrade-Insecure-Requests":"1"}
 
-flipkartHeaders = {"User-Agent":"Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/126.0.0.0 Safari/537.36", 
+flipkartHeaders = {"User-Agent":"Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:66.0) Gecko/20100101 Firefox/66.0", 
                     "Accept-Encoding":"gzip, deflate, br, zstd", 
                     "Accept":"text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7",
                     "Connection":"close", 
