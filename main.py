79dfc8eaed35b2631c7087b9b884ae06a9397c15
snande <satishnande23@gmail.com>
2024-07-17 17:52:34 +0530

    chaning flipkart header

--- main.py
+++ main.py
@@ -40,11 +40,12 @@ amazonHeaders = {"User-Agent":"Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:66.0
                 "Upgrade-Insecure-Requests":"1"}
 
 flipkartHeaders = {"User-Agent":"Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:66.0) Gecko/20100101 Firefox/66.0", 
-                    # "Accept-Encoding":"gzip, deflate", 
-                    # "Accept":"text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8",
-                    # "DNT":"1",
-                    # "Connection":"close", 
-                    # "Upgrade-Insecure-Requests":"1"
+                    "Accept-Encoding":"gzip, deflate", 
+                    "Accept":"text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8",
+                    "DNT":"1",
+                    "Connection":"close", 
+                    "Upgrade-Insecure-Requests":"1",
+                    "tz": 'GMT+05:30'
                     }
 
 num_prod_search = 160
